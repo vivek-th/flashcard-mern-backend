fa9afd7e9d308d9fb9256e5a0fa02253b03d5340
Vivek Thakur <vivekthakuruspc@gmail.com>
2023-10-07 22:37:15 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { config } from "dotenv";
+config();
+// mongoose.set('strictQuery', true);
+
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import { getDecksController } from "./controllers/getDecksController";
+import { createDeckController } from "./controllers/createDeckController";
+import { deleteDeckController } from "./controllers/deleteDeckController";
+import { getDeckController } from "./controllers/getDeckController";
+import { createCardForDeckController } from "./controllers/createCardForDeckController";
+import { deleteCardOnDeckController } from "./controllers/deleteCardOnDeckController";
+
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http:/localhost:5000", "https:/flashcard.onrender.com"],
+  })
+);
+app.use(express.json());
+
+app.get("/decks", getDecksController);
+app.post("/decks", createDeckController);
+app.delete("/decks/:deckId", deleteDeckController);
+app.get("/decks/:deckId", getDeckController);
+app.post("/decks/:deckId/cards", createCardForDeckController);
+app.delete("/decks/:deckId/cards/:index", deleteCardOnDeckController);
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`listening on port ${PORT}`);
+  app.listen(PORT);
+});
